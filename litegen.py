a0487ff743ef659f15a4940fe7c46da1938a1521
Sirawit Pongnakintr <s6007589@mwit.ac.th>
2019-10-01 00:15:55 +0700

    Fix wrong files

--- litegen.py
+++ litegen.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 import zipfile
+import shutil
 
 parser = argparse.ArgumentParser(description='Generate some testcases', formatter_class=argparse.RawDescriptionHelpFormatter, epilog='''
 Detailed Usage:
@@ -10,7 +11,7 @@ Solution File: sol.py, the file which contains the solution
 Generator File: gen.py, the file to generate input from parameters (read from stdin, output to stdout)
 Configuration File: conf.cfg, the file containing the data which will be sent to gen.py
     For N testcases, there should be N lines in the file, each line will be read by gen.py as input
-')
+''')
 args = parser.parse_args()
 
 # Procedure:
@@ -41,13 +42,17 @@ for i in range(n):
 os.mkdir('result')
 for i in range(n):
 	os.rename(str(i+1) + '.in', 'result/' + str(i+1) + '.in')
-	os.reanme(str(i+1) + '.sol', 'result/' + str(i+1) + '.sol')
+	os.rename(str(i+1) + '.sol', 'result/' + str(i+1) + '.sol')
 
 # 6.
 # ziph is zipfile handle
-ziph = zipfile.ZipFile('Package.zip', 'w', zipfile.ZIP_DEFLATED)
+ziph = zipfile.ZipFile('result.zip', 'w', zipfile.ZIP_DEFLATED)
 path = 'result/'
 for root, dirs, files in os.walk(path):
 	for file in files:
 		ziph.write(os.path.join(root, file))
 ziph.close()
+
+# 7. (Optional) Clean up
+shutil.rmtree('result')
+os.remove('tmpf')
